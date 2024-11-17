1f1cc4f841ef649da8422b3fc14f1565133befd9
alkad <alkad@DESKTOP-MRLFBEU>
2024-11-18 00:49:22 +0100

    Omar Commit.

--- oopversuch2last/src/factory/Product.java
+++
@@ -1,9 +0,0 @@
-package factory;
-
-import java.io.IOError;
-import java.io.IOException;
-
-public abstract class Product {
-	public abstract String [] leseAusDatei() throws IOException;
-	public abstract void schliesseDatei() throws IOException;
-}
